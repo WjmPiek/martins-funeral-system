898e177f41eb848895e41a64c2e4393da158f523
Wjm Piek <wjm@martinsdirect.com>
2026-06-17 17:44:06 +0200

    Fix franchise user import timeout

--- app/models.py
+++ app/models.py
@@ -85,10 +85,10 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(
-                                password,
-                                method="pbkdf2:sha256",
-                                salt_length=8
-                            )
+            password,
+            method="pbkdf2:sha256",
+            salt_length=8
+        )
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
