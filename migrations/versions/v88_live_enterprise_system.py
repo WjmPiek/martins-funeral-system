705252419bd5032d2b10ec1dd75a001c6b43688c
Wjm Piek <wjm@martinsdirect.com>
2026-07-02 09:57:20 +0200

    Stabilise 1

--- migrations/versions/v88_live_enterprise_system.py
+++ migrations/versions/v88_live_enterprise_system.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'v88_live_system'
-down_revision = "v87_import_ui"
+down_revision = 'v87_import_ui'
 branch_labels = None
 depends_on = None
 
